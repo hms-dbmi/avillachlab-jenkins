c5fa62e626b117aeb7c8d2bd4cbfad18fa4387ca
Jason Stedman <namdets@gmail.com>
2019-12-04 10:12:24 -0500

    no longer using docker-compose until it uses Python 3 by default due to the deprecation and EOL of Python 2 in January 2020

--- dev-jenkins-terraform/install-docker.sh
+++ dev-jenkins-terraform/install-docker.sh
@@ -3,12 +3,7 @@ echo "user-data progress starting update"
 sudo yum -y update 
 echo "user-data progress finished update installing epel-release"
 sudo yum -y install epel-release 
-echo "user-data progress finished epel-release starting python-pip"
-sudo yum -y install python-pip 
-sudo pip install --upgrade pip
-echo "user-data progress finished python-pip starting docker-compose"
-yes | sudo pip install --ignore-installed requests docker-compose
-echo "user-data progress finished docker-compose adding docker-ce repo"
+echo "user-data progress finished epel-release adding docker-ce repo"
 sudo yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 echo "user-data progress added docker-ce repo starting docker install"
 sudo yum -y install docker-ce docker-ce-cli containerd.io
@@ -17,5 +12,6 @@ sudo systemctl enable docker
 echo "user-data progress finished enabling docker service starting docker"
 sudo service docker start
 cd /home/centos/jenkins
-sudo docker-compose up -d
+sudo docker build -t avillach-lab-dev-jenkins .
+sudo docker run -d -v /var/run/docker.sock:/var/run/docker.sock -p 80:8080 --name jenkins --restart always avillach-lab-dev-jenkins
 echo "setup script finished"
