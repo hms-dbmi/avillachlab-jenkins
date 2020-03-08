6af12ae2ee7a4b812d9bcd9473e8eff860a21161
Paul Avillach <paul_avillach@hms.harvard.edu>
2020-03-08 16:02:42 -0400

    remove okta_saml_app_id

--- dev-jenkins-terraform/jenkins-ec2.tf
+++ dev-jenkins-terraform/jenkins-ec2.tf
@@ -74,7 +74,7 @@ output "entity_id" {
 data "template_file" "jenkins-config-xml" {
   template = file("../jenkins-docker/${var.config-xml-filename}")
   vars = {
-    okta_saml_app_id = regex("[[:alnum:]]+$", "${data.okta_app_metadata_saml.jenkins-saml.entity_id}")
+    // okta_saml_app_id = var.okta-saml-app-id
     aws_account_app = var.aws-account-app
     arn_role_app = var.arn-role-app
     arn_role_cnc = var.arn-role-cnc
