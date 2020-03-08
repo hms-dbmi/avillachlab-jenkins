257db32d4f8253c0dfadf8b761803646fa6e339c
Paul Avillach <paul_avillach@hms.harvard.edu>
2020-03-08 14:36:55 -0400

    add okta provider

--- dev-jenkins-terraform/provider.tf
+++ dev-jenkins-terraform/provider.tf
@@ -2,3 +2,8 @@ provider "aws" {
   region     = "us-east-1" 
   profile    = "avillachlab-secure-infrastructure"
 }
+
+provider "okta" {
+  org_name  = "hms-harvard-avillachlab"
+  base_url  = "okta.com"
+}
