f9fe9409ccb3b07e2db1dd4308b26949cd9f9747
Jason Stedman <namdets@gmail.com>
2020-09-24 18:04:09 -0400

    updating to latest centos CIS benchmark AMI

--- dev-jenkins-terraform/jenkins-ec2.tf
+++ dev-jenkins-terraform/jenkins-ec2.tf
@@ -31,7 +31,7 @@ data "template_cloudinit_config" "config" {
 }
 
 resource "aws_instance" "dev-jenkins" {
-  ami = "ami-000fb48d9b7b58b97"
+  ami = "ami-0901b7b7f72986a8f"
   instance_type = "m5.xlarge"
   associate_public_ip_address = false
   key_name = aws_key_pair.generated_key.key_name
