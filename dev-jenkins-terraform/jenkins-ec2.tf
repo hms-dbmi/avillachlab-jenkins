ec54d39586fad713704a4f4f1c63637a6fd690b8
Bhanu Prasad G <bhanu_prasad@hms.harvard.edu>
2021-02-18 06:10:01 -0500

    update new AMI

--- dev-jenkins-terraform/jenkins-ec2.tf
+++ dev-jenkins-terraform/jenkins-ec2.tf
@@ -31,7 +31,7 @@ data "template_cloudinit_config" "config" {
 }
 
 resource "aws_instance" "dev-jenkins" {
-  ami = "ami-0e8dc7ef34f9066c5"
+  ami = "ami-0bb5d98c40b312691"
   instance_type = "m5.xlarge"
   associate_public_ip_address = false
   key_name = aws_key_pair.generated_key.key_name
