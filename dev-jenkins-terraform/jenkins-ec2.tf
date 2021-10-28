ffa4b33333cf2c1c6ba59cb88b74ad27272587fa
Bhanu Prasad G <bp85@users.noreply.github.com>
2021-10-28 09:23:52 -0400

    update AMI ID

--- dev-jenkins-terraform/jenkins-ec2.tf
+++ dev-jenkins-terraform/jenkins-ec2.tf
@@ -31,7 +31,7 @@ data "template_cloudinit_config" "config" {
 }
 
 resource "aws_instance" "dev-jenkins" {
-  ami = "ami-0904f6a2b242cb1a7"
+  ami = "ami-086ad4194f2b84514"
   instance_type = "m5.2xlarge"
   associate_public_ip_address = false
   key_name = aws_key_pair.generated_key.key_name
