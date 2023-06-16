03a3a847bfa87845554da8f61032015f60257ac2
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2023-06-16 17:38:14 -0400

    updating location of user-script to more configurable

--- jenkins-terraform/jenkins-local-tf-variables.tf
+++ jenkins-terraform/jenkins-local-tf-variables.tf
@@ -8,6 +8,8 @@
 locals {
    # list of support Distributions.  Used by jenkins_tf_local_var_dist for validation of Supported Distributions
    valid_os = [ "CENTOS" ]
+   centos_user_script = "distro/linux/centos/user-scripts/install-docker.sh";
+   
    
    # set user script location using coalesce based on the distribution
    centos_script = var.jenkins_tf_local_var_OS_dist == "CENTOS" ? "user-scripts/linux/centos/install-docker.sh" : ""
