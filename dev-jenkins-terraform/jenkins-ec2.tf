33776ea2e491f965a6a8d9adefe28275d3081c35
Pluto Matters <45603814+funfunpluto@users.noreply.github.com>
2023-08-30 12:49:42 -0400

    Update jenkins-ec2.tf

--- dev-jenkins-terraform/jenkins-ec2.tf
+++ dev-jenkins-terraform/jenkins-ec2.tf
@@ -31,7 +31,7 @@ data "template_cloudinit_config" "config" {
 }
 
 resource "aws_instance" "dev-jenkins" {
-  ami = "ami-0c9ea50fdbda13499"
+  ami = "ami-0babe2276d5766efc"
   instance_type = "m5.2xlarge"
   associate_public_ip_address = false
   key_name = aws_key_pair.generated_key.key_name
