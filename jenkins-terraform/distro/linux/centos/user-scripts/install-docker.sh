c9a09316d6f5ee1e0923cbaa393b72bba374327c
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2023-10-02 18:15:09 -0400

    one dollar sign

--- jenkins-terraform/distro/linux/centos/user-scripts/install-docker.sh
+++ jenkins-terraform/distro/linux/centos/user-scripts/install-docker.sh
@@ -22,7 +22,7 @@ sudo docker run -d --log-driver syslog --log-opt tag=jenkins \
 #sudo docker logs -f jenkins > /var/log/jenkins-docker-logs/jenkins.log &
 
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")" --silent http://169.254.169.254/latest/meta-data/instance-id)
-sudo /usr/bin/aws --region=us-east-1 ec2 create-tags --resources $${INSTANCE_ID} --tags Key=InitComplete,Value=true
+sudo /usr/bin/aws --region=us-east-1 ec2 create-tags --resources ${INSTANCE_ID} --tags Key=InitComplete,Value=true
 
 
 while [ "$(aws --region=us-east-1 ec2 describe-tags --filters "Name=resource-id,Values=${INSTANCE_ID}" | jq -r '.Tags[] | select(.Key=="InitComplete") | .Value')" != "TRUE" ]
