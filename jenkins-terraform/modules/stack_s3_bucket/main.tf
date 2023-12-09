fe1c449ccc371c7c7cbaa1154238a327f9cc90aa
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2023-12-09 12:42:13 -0500

    remove retention

--- jenkins-terraform/modules/stack_s3_bucket/main.tf
+++ jenkins-terraform/modules/stack_s3_bucket/main.tf
@@ -3,11 +3,13 @@ provider "aws" {
   version = "5.30.0"
 }
 
-resource "aws_s3_bucket" "root" {
+resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 
   acl = "private"
-
+  
+  object_lock_enabled = true
+  
   versioning {
     enabled = var.versioning_enabled
   }
@@ -19,14 +21,13 @@ resource "aws_s3_bucket" "root" {
       }
     }
   }
-
-  object_lock_configuration {
-    object_lock_enabled = var.object_lock_enabled
-  }
-
 }
 
-resource "aws_s3_bucket_policy" "root_ac" {
-  bucket = aws_s3_bucket.root.bucket
+resource "aws_s3_bucket_object_lock_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.bucket
   policy = var.bucket_policy_json
 }
