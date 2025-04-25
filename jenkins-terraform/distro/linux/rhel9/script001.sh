84ee4bd4d4d33d178b3b2147b4e7f49b8be72b2c
thomas_desain1 <thomas_desain@hms-srce.org>
2025-04-25 09:23:11 -0400

    Add support for RHEL9 distribution in Jenkins Terraform setup
    Introduced a script to configure Jenkins on RHEL9, including Podman setup and systemd integration. Updated Terraform variables and AMI configuration to recognize and handle RHEL9 as a valid OS option.

---
+++ jenkins-terraform/distro/linux/rhel9/script001.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+echo "ENABLE_PODMAN=true" | sudo tee /opt/srce/startup.config
+#echo "SPLUNK_INDEX=?" | sudo tee /opt/srce/startup.config
+#echo "NESSUS_GROUP=swb-${env}-1" | sudo tee -a /opt/srce/startup.config
+
+sh /opt/srce/scripts/start-gsstools.sh
+sudo yum -y update
+
+# grab image tar
+aws s3 cp s3://${jenkins_tf_state_bucket}/containers/jenkins/jenkins.tar.gz jenkins.tar.gz
+
+# load image
+load_result=$(podman load -i jenkins.tar.gz)
+image_tag=$(echo "$load_result" | grep -o -E "jenkins:[[:alnum:]_]+")
+
+CONTAINER_NAME=jenkins
+
+podman rm -f $CONTAINER_NAME || true
+
+podman run -d --privileged \
+    --log-driver=journald \
+    --log-opt tag=jenkins \
+    -v /var/run/podman/podman.sock:/var/run/docker.sock \
+    -p 443:8443 \
+    --name $CONTAINER_NAME \
+    --env GITLAB_USER=$GITLAB_USER \
+    --env GITLAB_TOKEN=$GITLAB_TOKEN \
+    $image_tag
+
+# systemd setup.
+podman generate systemd --name $CONTAINER_NAME --restart-policy=always --files
+
+sudo mv container-$CONTAINER_NAME.service /etc/systemd/system/
+
+sudo restorecon -v /etc/systemd/system/container-$CONTAINER_NAME.service
+
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl enable container-$CONTAINER_NAME.service
+sudo systemctl restart container-$CONTAINER_NAME.service
+
+echo "Verifying container-$CONTAINER_NAME.service status..."
+sudo systemctl is-enabled container-$CONTAINER_NAME.service
+sudo systemctl status container-$CONTAINER_NAME.service --no-pager
+
+#sudo docker logs -f jenkins > /var/log/jenkins-docker-logs/jenkins.log &
+
+INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")" --silent http://169.254.169.254/latest/meta-data/instance-id)
+sudo /usr/bin/aws --region=us-east-1 ec2 create-tags --resources $${INSTANCE_ID} --tags Key=InitComplete,Value=true
+
--- jenkins-terraform/jenkins-local-tf-variables.tf
+++ jenkins-terraform/jenkins-local-tf-variables.tf
@@ -7,14 +7,17 @@
 
 locals {
   # list of supported Distributions.  Used by jenkins_tf_local_var_dist for validation of Supported Distributions
-  valid_os           = ["CENTOS"]
+  valid_os           = ["CENTOS", "RHEL9"]
+
   centos_user_script = "distro/linux/centos/user-scripts/install-docker.sh"
+  rhel9_user_script = "distro/linux/rhel9/user-scripts/script001.sh"
 
   # set user script location using coalesce based on the distribution
   centos_script  = var.jenkins_tf_local_var_OS_dist == "CENTOS" ? local.centos_user_script : ""
+  rhel9_script  = var.jenkins_tf_local_var_OS_dist == "RHEL9" ? local.rhel9_user_script : ""
   example_script = var.jenkins_tf_local_var_OS_dist == "EXAMPLE" ? "Unreachable Coalesece Example" : ""
 
-  user_script = coalesce(local.centos_script, local.example_script)
+  user_script = coalesce(local.centos_script, local.rhel9_script, local.example_script)
 
   project = var.env_is_open_access ? "Open PIC-SURE" : "Auth PIC-SURE"
 }
